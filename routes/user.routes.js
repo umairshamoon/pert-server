a8832b5e69cb45b88e82c0f85d3ca85b9183f33e
umairshamoon <umairshamoon513@gmail.com>
2022-11-10 17:02:01 +0500

    engines

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,14 +3,9 @@ const router = express.Router()
 const user = require('../controllers/user.controller')
 //middlewares
 const upload = require('../middleware/multer.middleware')
-const { isLogin } = '../middleware'
+
 router.post('/login', user.login)
 router.post('/register', upload.single('profile'), user.register)
-router.post(
-  '/sell/pet',
-  isLogin,
-  upload.single('pet'),
-  user.sellPet
-)
+router.post('/sell/pet', upload.single('pet'), user.sellPet)
 
 module.exports = router
