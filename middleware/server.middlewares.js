3dfbdc6f0719907dae5af4ca78b7c836b9526786
umairshamoon <umairshamoon513@gmail.com>
2022-11-10 16:56:43 +0500

    engines

--- middleware/server.middlewares.js
+++ middleware/server.middlewares.js
@@ -1,8 +1,12 @@
 const cors = require('cors')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
 
 module.exports = function (app, express) {
   app.use(express.urlencoded({ extended: false }))
+  app.use(helmet())
+  app.use(compression())
   app.use(cors())
   app.use(morgan('dev'))
 }
