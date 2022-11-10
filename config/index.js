c48351318857c75debdedf5f1e843f855743a527
umairshamoon <umairshamoon513@gmail.com>
2022-11-10 16:57:15 +0500

    engines

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 module.exports = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
